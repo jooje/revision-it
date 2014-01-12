412fdaf6bb0520f903fed9e4d3340afdbb4e40d4
mzp <mzpppp@gmail.com>
2014-01-12 15:12:06 +0000

    bundle exec rails new . -T --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RevisionIt::Application.config.secret_key_base = 'ad7792e8bfd14c99ae99656b605bfe27e37d1f9a1e8b4796111bb7d7d8ee3dfec7193c1dfcfa252aa069100625e730a62fe7687b376c4345c0c2bf24b34ad2bc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RevisionIt::Application.config.session_store :cookie_store, key: '_revision-it_session'
